6709455acf3ea717bde2cc9c947bb3607314e33c
wanglin.zj <wanglin.zj@alibaba-inc.com>
2023-05-30 11:18:20 +0800

    [to #50036702] feat: support obtaining version from file.
    support obtaining version from file.
    Link: https://code.alibaba-inc.com/torchx/rlhf/codereview/12788167

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
+import sys
 from setuptools import setup, find_packages
+
+if sys.version_info[0] < 3:
+  import imp
+  VERSION = imp.load_source('rlhf.version', 'rlhf/utils/version.py').VERSION
+else:
+  from importlib.machinery import SourceFileLoader
+  VERSION = SourceFileLoader("rlhf.version", "rlhf/utils/version.py") \
+      .load_module().VERSION
 
 setup(
     name='rlhf',
-    version='0.1.0',
+    version=VERSION,
     python_requires='>3.6.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
+    author='Aliyun Inc.',
+    license='Apache 2.0',
 )
