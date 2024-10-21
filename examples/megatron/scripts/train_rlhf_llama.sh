0b7ece7d715945dc2e618802124d793ad2c3298c
jiangle.jl <jiangle.jl@alibaba-inc.com>
2024-10-21 11:13:25 +0800

    fix

--- examples/megatron/scripts/train_rlhf_llama.sh
+++ examples/megatron/scripts/train_rlhf_llama.sh
@@ -59,7 +59,7 @@ if [[ "$model_size" == "llama2-7B" ]]; then
     export reward_generation_batch_size=64
     export train_micro_batch_size=16
     export max_num_batched_tokens=65536
-    export gpu_memory_utilization=0.9
+    export gpu_memory_utilization=0.8
     export num_gpu_ref=4
     export num_gpu_value=4
     export num_gpu_ppo_policy=4
