ae7e8646310fc93a6aa55807c7f40b5f03bd5d42
xianyan.xianyanjia <xianyan.xianyanjia@alibaba-inc.com>
2024-11-20 13:01:02 +0800

    fix comment

--- chatlearn/models/vllm_module_v2.py
+++ chatlearn/models/vllm_module_v2.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""VLLM module"""
+"""VLLM module v2"""
 
 import asyncio
 import inspect
