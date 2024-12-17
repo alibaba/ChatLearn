2747c6335e5e029b4fb3835d5371c0162120c010
jiangle.jl <jiangle.jl@alibaba-inc.com>
2024-12-17 09:57:02 +0800

    fix pylint

--- chatlearn/models/vllm/hooks/__init__.py
+++ chatlearn/models/vllm/hooks/__init__.py
@@ -28,7 +28,7 @@ if is_vllm_v2():
 else:
     if importlib.util.find_spec("vllm"):
         import vllm
-        from chatlearn.utils.constant import CURRENT_VLLM_VERSION, VLLMVersion # pylint: disable=uungrouped-imports
+        from chatlearn.utils.constant import CURRENT_VLLM_VERSION, VLLMVersion # pylint: disable=ungrouped-imports
         if CURRENT_VLLM_VERSION == VLLMVersion.v_0_3_0:
             from chatlearn.models.vllm.hooks import sampler
         elif CURRENT_VLLM_VERSION in [VLLMVersion.v_0_5_1, VLLMVersion.v_0_6_3]:
