58f8ec85b087363d7326388cdd8fed10d9f28085
jiangle.jl <jiangle.jl@alibaba-inc.com>
2025-02-04 16:43:38 +0800

    Support batch shuffling for trainer.

--- chatlearn/utils/arguments.py
+++ chatlearn/utils/arguments.py
@@ -21,7 +21,7 @@ from typing import List
 
 import yaml
 
-from chatlearn.utils.constant import LORA_LAYER, RAY_PG_STRATEGY, PARAM_SYNC_COMM_TYPE, ROUTED_EXPERT_REGROUPING_COMM_TYPE
+from chatlearn.utils.constant import LORA_LAYER, RAY_PG_STRATEGY, PARAM_SYNC_COMM_TYPE, ROUTED_EXPERT_REGROUPING_COMM_TYPE, TrainingShffuleMode
 from chatlearn.utils.logger import logger
 from chatlearn.utils.utils import get_attributes
 
@@ -319,6 +319,8 @@ class RuntimeConfig(BaseConfig):
     max_relay_episode: int = 0
     #: relay after n episodes
     relay_episode_offset: int = 0
+    #: training shuffle mode
+    training_shuffle_mode: str = TrainingShffuleMode.BATCH
     #: consumed samples
     consumed_samples: int = 0
     #: concurrent model setup
