e3d145f97983f144b82d915cc4a5220cd4f9d789
yanhaiqiang.yhq <yanhaiqiang.yhq@alibaba-inc.com>
2025-05-29 05:56:15 +0000

    fix adv compute bug

--- examples/fsdp/entry/train_grpo.py
+++ examples/fsdp/entry/train_grpo.py
@@ -48,6 +48,7 @@ def compute_grpo_adv(episode_relay_buffers):
     buffers = episode_relay_buffers[-1].buffer
     queryids2samples = defaultdict(list)
     for s in buffers:
+        queryids2samples[','.join(map(str, s["prompt_token_ids"]))].append(s)
     
     res_buffers = []
     for _, l in queryids2samples.items():
