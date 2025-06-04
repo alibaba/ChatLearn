7959660e3d6c74abf93c88c6fb7b311face27a3b
yanhaiqiang.yhq <yanhaiqiang.yhq@alibaba-inc.com>
2025-06-04 14:59:48 +0800

    update

--- chatlearn/models/vllm_module.py
+++ chatlearn/models/vllm_module.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""VLLM module for """
+"""VLLM module"""
 
 import inspect
 import os
