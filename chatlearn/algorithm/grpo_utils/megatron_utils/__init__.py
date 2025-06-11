eb4bb8814a089a839095317ccec30a8282a58f8c
yanhaiqiang.yhq <yanhaiqiang.yhq@alibaba-inc.com>
2025-06-11 15:19:53 +0800

    add

--- chatlearn/algorithm/grpo_utils/megatron_utils/__init__.py
+++ chatlearn/algorithm/grpo_utils/megatron_utils/__init__.py
@@ -1,3 +1,4 @@
+# pylint: skip-file
 from .policy_model import PolicyModel
 from .tokenizer_utils import build_tokenizer
 from .train_helper import forward_step, inference_forward_step, training_log
