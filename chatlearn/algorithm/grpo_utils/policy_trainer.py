94259c199a808ed0930338afa3153c15349cd9a1
yanhaiqiang.yhq <yanhaiqiang.yhq@alibaba-inc.com>
2025-07-10 10:41:28 +0800

    rm unused code

--- chatlearn/algorithm/grpo_utils/policy_trainer.py
+++ chatlearn/algorithm/grpo_utils/policy_trainer.py
@@ -246,7 +246,6 @@ class PolicyTrainer(FSDPModule):
             pg_loss_mean = torch.sum(pg_loss) / response_token_length_total * self.fsdp_size
             if self.module_args.entropy_coef > 0:
                 pg_loss_mean = pg_loss_mean - self.module_args.entropy_coef * entropy_loss_mean
-                # pg_loss_mean = -1*self.module_args.entropy_coef * entropy_loss_mean
             if self.module_args.kl_coef > 0:
                 pg_loss_mean = pg_loss_mean + self.module_args.kl_coef * kl_loss_mean
             pg_loss_mean.backward()
