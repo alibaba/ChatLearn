832dfb98a9fc11d98807d0c7463db90fc81102d7
Peng Li <jerryli1981@gmail.com>
2025-08-04 03:04:29 +0000

    Add GSPO for Mcore Models

--- chatlearn/algorithm/grpo_utils/loss_gallery.py
+++ chatlearn/algorithm/grpo_utils/loss_gallery.py
@@ -1,6 +1,7 @@
 """pg loss"""
 import torch
 
+
 def calculate_grpo_loss(
     log_probs: torch.Tensor,
     old_log_probs: torch.Tensor,
@@ -8,27 +9,19 @@ def calculate_grpo_loss(
     diff_clip_ratio: float = 10,
     pos_clip_ratio: float = 0.2,
     neg_clip_ratio: float = 0.2,
-    final_clip_ratio: float = 0.01
+    final_clip_ratio: float = 0.01,
 ):
-
     logprobs_diff = log_probs - old_log_probs
     # clip logprobs_diff before exp to avoid overflow
     logprobs_diff = torch.clamp(logprobs_diff, max=diff_clip_ratio)
 
     ratio = torch.exp(logprobs_diff)
-    advantages.unsqueeze_(-1)
-
-    pg_loss = -advantages * ratio
+    pg_loss = -advantages.unsqueeze(-1) * ratio
     # Upper and lower bound clip
-    is_positive_clipped = (ratio > (1 + pos_clip_ratio)) * (advantages > 0)
-    is_negative_clipped = (ratio < (1 - neg_clip_ratio)) * (advantages < 0)
-
-    pg_loss_2 = -advantages * torch.clamp(
+    pg_loss_2 = -advantages.unsqueeze(-1) * torch.clamp(
         ratio, 1 - neg_clip_ratio, 1 + pos_clip_ratio
     )
     pg_loss_clip = torch.max(pg_loss, pg_loss_2)
-
-    is_clipped = pg_loss_2 > pg_loss
     pg_loss_upperbound = torch.ones_like(pg_loss) * final_clip_ratio
     # final clip on loss
     loss = torch.min(pg_loss_clip, pg_loss_upperbound)
