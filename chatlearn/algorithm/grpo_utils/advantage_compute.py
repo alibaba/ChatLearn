fix adv calculation

chatlearn/algorithm/grpo_utils/advantage_compute.py
@@ -1,8 +1,29 @@
 """compute advantage for grpo"""
 from typing import List, Dict, Any
+from collections import defaultdict
+import numpy as np
 
-def compute_grpo_adv(episode_replay_buffers: List[Dict[str, Any]]):
-    buffers = episode_replay_buffers[-1].buffer
-    for s in buffers:
-        s["advantages"] = (s["rule_reward"] - s["rule_reward_mean"]) / (s["rule_reward_std"] + 1e-5)
-    return buffers
+class AdvantageComputer:
+    def __init__(self, num_inference_per_prompt):
+        self.rule_reward_buffer = defaultdict(list)
+        self.num_inference_per_prompt = num_inference_per_prompt
+
+    def __call__(self, episode_replay_buffers: List[Dict[str, Any]]):
+        buffers = episode_replay_buffers[-1].buffer
+        # Update buffer first
+        for s in buffers:
+            sample_id = s['prompt_uid']
+            self.rule_reward_buffer[sample_id].append(s["rule_reward"])
+
+        # Calculate advantage for all samples
+        for s in buffers:
+            sample_id = s['prompt_uid']
+            avg = np.mean(self.rule_reward_buffer[sample_id])
+            std = np.std(self.rule_reward_buffer[sample_id])
+            s['advantages'] = (s["rule_reward"] - avg) / (std + 1e-5)
+        
+        # clean buffer
+        pop_keys = [k for k,v in self.rule_reward_buffer.items() if len(v) == self.num_inference_per_prompt]
+        for key in pop_keys:
+            self.rule_reward_buffer.pop(key)
+        return buffers

chatlearn/models/reward/rule_reward.py
@@ -39,8 +39,6 @@ class RuleReward(BaseModule):
         assert self.total_gpu == 0, "RuleReward does not require GPU"
         self._num_gpu_per_replica = 0
         self._num_replica = self.module_args.num_cpu // self.module_args.cpu_per_process
-        self.rule_reward_buffer = defaultdict(list)
-        self.num_inference_per_prompt = self.module_args.num_inference_per_prompt
 
     def setup(self):
         self.stats = {}
@@ -56,22 +54,9 @@ class RuleReward(BaseModule):
             data_source = data_b["data_source"]
             ground_truth = data_b["ground_truth"]
             compute_score_fn = self.select_rule_reward_score_fn(data_source)
-            single_score = compute_score_fn(str_output, ground_truth)
-            reward.append(single_score)
-            self.rule_reward_buffer[hash(data_b['prompt'])].append(single_score)
+            reward.append(compute_score_fn(str_output, ground_truth))
             data_b.update({"rule_reward": reward[-1], "eval_source": data_source})
-        # Update mean/std for single prompt
-        for data_b in data:
-            reward_list = self.rule_reward_buffer[hash(data_b['prompt'])]
-            data_b.update({"rule_reward_mean": np.mean(reward_list), "rule_reward_std": np.std(reward_list)})
-        self.clean_dict()
         return data, reward
-
-    def clean_dict(self):
-        # when all response for prompt is finished, pop those key out
-        pop_keys = [k for k,v in self.rule_reward_buffer.items() if len(v) == self.num_inference_per_prompt]
-        for key in pop_keys:
-            self.rule_reward_buffer.pop(key)
 
     @compute_decorator(trainable=False, rollout=False)
     @timeit()