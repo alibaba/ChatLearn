53836e55d7cbc8e96fd71a14e75ec4fd06b8999d
zouxinyi0625 <zouxinyi.zxy@alibaba-inc.com>
2025-09-03 17:00:45 +0800

    remove useless patch

--- chatlearn/models/patches/transformers/qwen2_5_vl_patch.py
+++ chatlearn/models/patches/transformers/qwen2_5_vl_patch.py
@@ -1,17 +1,11 @@
 """
 vl patch for packing
 """
-import inspect
 from typing import Optional, Tuple
 import torch
 from transformers.models.qwen2_5_vl.modeling_qwen2_5_vl import apply_multimodal_rotary_pos_emb, repeat_kv
-from transformers.modeling_flash_attention_utils import fa_peft_integration_check, _upad_input
+from transformers.modeling_flash_attention_utils import _flash_attention_forward
 from transformers.cache_utils import Cache
-from flash_attn import flash_attn_func, flash_attn_varlen_func
-from flash_attn.bert_padding import pad_input
-
-
-_flash_supports_window_size = "window_size" in list(inspect.signature(flash_attn_func).parameters)
 
 def get_rope_index(
         processor,
@@ -144,123 +138,6 @@ def get_rope_index(
         return position_ids, mrope_position_deltas
 
 
-
-def _flash_attention_forward(
-    query_states: torch.Tensor,
-    key_states: torch.Tensor,
-    value_states: torch.Tensor,
-    attention_mask: Optional[torch.Tensor],
-    query_length: int,
-    is_causal: bool,
-    dropout: float = 0.0,
-    position_ids: Optional[torch.Tensor] = None,
-    softmax_scale: Optional[float] = None,
-    sliding_window: Optional[int] = None,
-    use_top_left_mask: bool = False,
-    softcap: Optional[float] = None,
-    deterministic: Optional[bool] = None, # pylint: disable=unused-argument
-    cu_seq_lens_q: Optional[torch.LongTensor] = None,
-    cu_seq_lens_k: Optional[torch.LongTensor] = None,
-    max_length_q: Optional[int] = None,
-    max_length_k: Optional[int] = None,
-    target_dtype: Optional[torch.dtype] = None,
-    **kwargs, # pylint: disable=unused-argument
-):
-    """
-    copy from transformers/modeling_flash_attention_utils.py to use self-defined prepare_fa2_from_position_ids
-
-    Calls the forward method of Flash Attention - if the input hidden states contain at least one padding token
-    first unpad the input, then computes the attention scores and pad the final attention scores.
-    """
-    if not use_top_left_mask:
-        causal = is_causal
-    else:
-        # TODO: Remove the `query_length != 1` check once Flash Attention for RoCm is bumped to 2.1.
-        causal = is_causal and query_length != 1
-
-    # Assuming 4D tensors, key_states.shape[1] is the key/value sequence length (source length).
-    use_sliding_windows = (
-        _flash_supports_window_size and sliding_window is not None and key_states.shape[1] > sliding_window
-    )
-    flash_kwargs = {"window_size": (sliding_window, sliding_window)} if use_sliding_windows else {}
-
-    if softcap is not None:
-        flash_kwargs["softcap"] = softcap
-
-    # PEFT possibly silently casts tensors to fp32, this potentially reconverts to correct dtype or is a no op
-    query_states, key_states, value_states = fa_peft_integration_check(
-        query_states, key_states, value_states, target_dtype
-    )
-
-    # Contains at least one padding token in the sequence
-    if attention_mask is not None:
-        batch_size = query_states.shape[0]
-        query_states, key_states, value_states, indices_q, cu_seq_lens, max_seq_lens = _upad_input(
-            query_states, key_states, value_states, attention_mask, query_length
-        )
-        cu_seqlens_q, cu_seqlens_k = cu_seq_lens
-        max_seqlen_in_batch_q, max_seqlen_in_batch_k = max_seq_lens
-
-        attn_output_unpad = flash_attn_varlen_func(
-            query_states,
-            key_states,
-            value_states,
-            cu_seqlens_q=cu_seqlens_q,
-            cu_seqlens_k=cu_seqlens_k,
-            max_seqlen_q=max_seqlen_in_batch_q,
-            max_seqlen_k=max_seqlen_in_batch_k,
-            dropout_p=dropout,
-            softmax_scale=softmax_scale,
-            causal=causal,
-            **flash_kwargs,
-        )
-        attn_output = pad_input(attn_output_unpad, indices_q, batch_size, query_length)
-
-    # If position_ids is provided and check all examples do not contain only 1 sequence, If tensor in increasing
-    # then we probably have one sequence, otherwise it is packed. Additionally check we are in pre-fill/training stage.
-    # Use `flash_attn_varlen_func` to prevent cross-example attention and also allow padding free approach
-    elif position_ids is not None and (
-        max_length_q is not None or (query_length != 1 and not (torch.diff(position_ids, dim=-1) >= 0).all())
-    ):
-        batch_size = query_states.size(0)
-
-        if cu_seq_lens_q is None or cu_seq_lens_k is None:
-            query_states, key_states, value_states, indices_q, cu_seq_lens, max_seq_lens = (
-                prepare_fa2_from_position_ids(query_states, key_states, value_states, position_ids)
-            )
-
-            cu_seq_lens_q, cu_seq_lens_k = cu_seq_lens
-            max_length_q, max_length_k = max_seq_lens
-
-        else:
-            query_states = query_states.reshape(-1, query_states.size(-2), query_states.size(-1))
-            key_states = key_states.reshape(-1, key_states.size(-2), key_states.size(-1))
-            value_states = value_states.reshape(-1, value_states.size(-2), value_states.size(-1))
-
-        attn_output = flash_attn_varlen_func(
-            query_states,
-            key_states,
-            value_states,
-            cu_seqlens_q=cu_seq_lens_q,
-            cu_seqlens_k=cu_seq_lens_k,
-            max_seqlen_q=max_length_q,
-            max_seqlen_k=max_length_k,
-            dropout_p=dropout,
-            softmax_scale=softmax_scale,
-            causal=causal,
-            **flash_kwargs,
-        )
-
-        attn_output = attn_output.view(batch_size, -1, attn_output.size(-2), attn_output.size(-1))
-
-    else:
-        attn_output = flash_attn_func(
-            query_states, key_states, value_states, dropout, softmax_scale=softmax_scale, causal=causal, **flash_kwargs
-        )
-
-    return attn_output
-
-
 def prepare_fa2_from_position_ids(query, key, value, position_ids):
     """
     change vl position ids to fa2 format
@@ -284,8 +161,6 @@ def prepare_fa2_from_position_ids(query, key, value, position_ids):
     # =========================================================================
 
     return (query, key, value, indices_q, (cu_seq_lens, cu_seq_lens), (max_length, max_length))
-
-
 def Qwen2_5_VLFlashAttention2_patched_forward(
     self,
     hidden_states: torch.Tensor,
@@ -383,9 +258,12 @@ def Qwen2_5_VLFlashAttention2_patched_forward(
 
     return attn_output, attn_weights, past_key_value
 
-
 def apply_qwenvl_patch():
     # pylint: disable=import-outside-toplevel
     from transformers.models.qwen2_5_vl.modeling_qwen2_5_vl import Qwen2_5_VLFlashAttention2
     Qwen2_5_VLFlashAttention2.forward = Qwen2_5_VLFlashAttention2_patched_forward
+
+    # add patch to transformers
+    import transformers
+    transformers.modeling_flash_attention_utils.prepare_fa2_from_position_ids = prepare_fa2_from_position_ids
     
