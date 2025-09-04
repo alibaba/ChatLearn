ab5e9b5e85544625c2da43d1105c689b237d24c3
yytang0204 <yytang0220@outlook.com>
2025-09-04 14:57:36 +0800

    Add explain for data dict

--- chatlearn/data/prompt_dataset.py
+++ chatlearn/data/prompt_dataset.py
@@ -55,7 +55,7 @@ class PromptPipeline(Dataset):
                     enable_thinking=enable_thinking,
                 )
             input_ids = self.tokenizer.encode(prompt)
-            # When partial rollout enabled: 
+            # When partial rollout enabled:
             # input_ids may change (contain response tokens from previous rollouts)
             # prompt_token_ids will always be origial prompt tokens
             processed_data = {
