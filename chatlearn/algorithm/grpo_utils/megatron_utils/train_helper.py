7ebbf1d2b646f8169a08bfc069091e6353cb2845
lostkevin <lostkevin666@gmail.com>
2025-09-10 16:13:49 +0800

    fix ori_seq_len

--- chatlearn/algorithm/grpo_utils/megatron_utils/train_helper.py
+++ chatlearn/algorithm/grpo_utils/megatron_utils/train_helper.py
@@ -215,10 +215,10 @@ def reduce_from_context_parallel_region(
     is_packing: bool = False,
     inputs: Dict[str, Any] = None
 ):
+    if mpu.get_context_parallel_world_size() > 1:
+        logprobs = _reduce_from_tensor_model_parallel_region(logprobs, inputs['seq_indices'])
 
     if is_packing:
-        if mpu.get_context_parallel_world_size() > 1:
-            logprobs = _reduce_from_tensor_model_parallel_region(logprobs, inputs['seq_indices'])
         logprobs = pad_input(
             logprobs.permute(1, 0),
             inputs['indices'],
@@ -226,12 +226,7 @@ def reduce_from_context_parallel_region(
             inputs['ori_seq_len'],
         ).squeeze(-1)
 
-    else:
-        if mpu.get_context_parallel_world_size() > 1:
-            logprobs = _reduce_from_tensor_model_parallel_region(logprobs, inputs['all_token_position_ids'])
-
-        logprobs = logprobs[:, :logprobs.shape[1] - inputs['pad_size']]
-
+    logprobs = logprobs[:, :logprobs.shape[1] - inputs['pad_size']]
     return logprobs
 
 
@@ -323,11 +318,12 @@ def get_batch(
             "all_token_position_ids": None,
             "labels": labels_on_this_cp_rank.transpose(0, 1),
             'packed_seq_params': packed_seq_params,
-            "ori_seq_len": seqlen,
+            "ori_seq_len": seqlens_in_batch_padded.max(),
             "ori_batch_size": mbs,
             "seq_indices": seq_indices.unsqueeze(0), # [1, total_nnz_per_cp_rank]
             "indices": indices,
             "num_tokens_on_this_cp_rank": num_tokens_on_this_cp_rank,
+            'pad_size': seqlens_in_batch_padded.max() - seqlen
         }
 
     else:
@@ -384,7 +380,7 @@ def get_batch(
                 'packed_seq_params': None,
                 "ori_seq_len": None,
                 "ori_batch_size": None,
-                "seq_indices": None,
+                "seq_indices": chunked_dataset['position_ids'].clone(),
                 "indices": None,
                 "pad_size": pad_size,
                 "num_tokens_on_this_cp_rank": chunked_dataset['loss_mask'].sum(),
